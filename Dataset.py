55e7ccca678615b9aa71f634c6a9112ed8f1e8cb
ani <amazinganirudhhere@gmail.com>
2023-09-24 14:23:01 +0530

    creating a dataset from the web

---
+++ Dataset.py
@@ -0,0 +1,17 @@
+from bing_image_downloader import downloader
+
+def download_images(query, num_images=10):
+    try:
+        query_string = query
+        downloader.download(query_string, limit=num_images, adult_filter_off=True, force_replace=False)
+        print(f"Downloaded {num_images} images for {query}")
+    except Exception as e:
+        print(f"Error downloading images for {query}: {str(e)}")
+
+def main():
+    plants = ["tulasi", "alovera"]
+    for plant in plants:
+        download_images(plant)
+
+if __name__ == "__main__":
+    main()
